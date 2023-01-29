e44a4a7e36ff232607afbf385bed29d73572a9e1
BoredCreator <63941499+BoredCreator@users.noreply.github.com>
2023-01-28 23:46:52 -0800

    Basic App Finished. Polish Needed

--- zeroResponder/ZeroResponder/src/screens/home/map/index.js
+++ zeroResponder/ZeroResponder/src/screens/home/map/index.js
@@ -7,39 +7,56 @@ import {
   Animated,
   TouchableOpacity,
 } from "react-native";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 import { StackActions, useNavigation } from "@react-navigation/native";
 import * as Icon from "react-native-feather";
 import { getEmergencies } from "../../../redux/actions";
 export default function MapScreen() {
-  const [markerArray, setMarkerArray] = useState(null);
+  const [markerArrayS, setMarkerArrayS] = useState([]);
 
-  console.log("HEK");
-
-  const DisplayMarkers = (item) => {
-    let LatLng = {
-      latitude: 40,
-      longitude: -120,
-    };
-    return <Marker coordinate={LatLng} title={"Beware the opps"} />;
-  };
   useEffect(() => {
     async function fetchData() {
-      let temp = await getEmergencies();
-      setMarkerArray(temp);
+      let markerArray = [];
+      await firebase
+        .firestore()
+        .collection("emergencies")
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            markerArray.push({
+              title: doc.data().emergencyType,
+              coordinates: {
+                latitude: doc.data().latitude,
+                longitude: doc.data().longitude,
+              },
+            });
+          });
+        })
+        .catch((e) => console.log(e));
+
+      setMarkerArrayS(markerArray);
     }
     const response = fetchData();
-
-    console.log(
-      JSON.stringify(markerArray) + "IN OUR MAP FILE QQQQQQQQQQQQQQQQ"
-    );
   }, []);
 
+  markerArrayS.forEach((element) => {
+    console.log(element.coordinates);
+    console.log(element.title);
+  });
+  const tokyoRegion = {
+    latitude: 35.6762,
+    longitude: 139.6503,
+  };
   return (
-    <MapView
-      showsUserLocation={true}
-      style={{ height: "100%", width: "100%" }}
-    ></MapView>
+    <MapView showsUserLocation={true} style={{ height: "100%", width: "100%" }}>
+      {markerArrayS.map((element) => (
+        <Marker coordinate={element.coordinates} title={element.title} />
+      ))}
+      <Marker coordinate={tokyoRegion} />
+    </MapView>
   );
 }
